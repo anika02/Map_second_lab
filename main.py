b3f9528767cda0cbe9f8af6a46e680461d03148d
anika02 <anika.korabliova@gmail.com>
2020-02-18 19:26:25 +0200

    Add population

--- main.py
+++ main.py
@@ -42,18 +42,22 @@ def input_from_file(file_name, year):
     return lst
 
 
-location_user = [49.817545, 24.023932]
-loc_1 = input().split('.')
-loc_2 = input().split('.')
+year = input("Введіть рік:")
+loc_1 = input("Введіть широту:").split('.')
+loc_2 = input("Введіть довготу:").split('.')
 loc_1 = int(loc_1[0]) + int(loc_1[1])/(10 ^ (len(loc_1[1])))
 loc_2 = int(loc_2[0]) + int(loc_2[1])/(10 ^ (len(loc_2[1])))
 
-lst = input_from_file("C:\sec_semester\locations.list", '1916')
+locations_path = input(
+    "Введіть шлях до файлу з фільмами(якщо це інший файл: він має мати таку ж конструкцію як і даний файл):")
+world_path = input(
+    "Введіть шлях до файлу з населенням(якщо це інший файл: він має мати таку ж конструкцію як і даний файл):")
+lst = input_from_file(locations_path, year)
 print(len(lst))
 map_1 = folium.Map(location=[loc_1, loc_2], zoom_start=50)
 lst = lst[:50]
 
-geolocator = Nominatim(user_agent="specify_your_app_name_here", timeout= 3)
+geolocator = Nominatim(user_agent="specify_your_app_name_here", timeout=3)
 result_lst = []
 for i in lst:
     try:
@@ -61,26 +65,23 @@ for i in lst:
             i[2]).longitude])
     except:
         pass
-result_lst.sort(key=lambda x: haversine.haversine((loc_1, loc_2),(x[1], x[2])))#(x[1]-loc_1)**2 + (x[2]-loc_2)**2)
+result_lst.sort(key=lambda x: haversine.haversine(
+    (loc_1, loc_2), (x[1], x[2])))
 result_lst = result_lst[:10]
 
 
-fg = folium.FeatureGroup(name="first_map")
+fg = folium.FeatureGroup(name=year + "- films")
 for name, lt, ln in result_lst:
-    fg.add_child(folium.Marker(location= [lt, ln], popup= name, icon=folium.Icon()))
+    fg.add_child(folium.Marker(
+        location=[lt, ln], popup=name, icon=folium.Icon()))
 map_1.add_child(fg)
+
+fg_pp = folium.FeatureGroup(name="Population")
+fg_pp.add_child(folium.GeoJson(data=open(world_path, 'r', encoding='utf-8-sig').read(),
+                               style_function=lambda x: {'fillColor': 'green'
+                                                         if x['properties']['POP2005'] < 10000000 else 'orange'
+                                                         if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+map_1.add_child(fg_pp)
+map_1.add_child(folium.LayerControl())
+
 map_1.save('Map_1.html')
-
-# data = pandas.read_csv("Stan_1900.csv", error_bad_lines=False)
-# lat = data['lat']
-# lon = data['lon']
-# map = folium.Map(location=[48.314775, 25.082925], zoom_start=10)
-# fg = folium.FeatureGroup(name="Kosiv map")
-# for lt, ln in zip(lat, lon):
-#     fg.add_child(folium.Marker(
-#         location=[lt, ln], popup="1900 рік", icon=folium.Icon()))
-# map.add_child(fg)
-# map.save('Map_5.html')
-
-# lst = input_from_file("C:\sec_semester\locations.list", '1962')
-# print(len(lst))
