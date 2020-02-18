6174cf28a03ae0360d5c5ab1e67b2b0daffbe02d
anika02 <anika.korabliova@gmail.com>
2020-02-18 20:26:15 +0200

    New file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import folium
-import geopy
 from geopy.geocoders import Nominatim
 import haversine
 
@@ -41,47 +40,52 @@ def input_from_file(file_name, year):
     f.close()
     return lst
 
+try:
+    year = input("Введіть рік:")
+    loc_1 = input("Введіть широту:").split('.')
+    loc_2 = input("Введіть довготу:").split('.')
+    loc_1 = int(loc_1[0]) + int(loc_1[1])/(10 ^ (len(loc_1[1])))
+    loc_2 = int(loc_2[0]) + int(loc_2[1])/(10 ^ (len(loc_2[1])))
 
-year = input("Введіть рік:")
-loc_1 = input("Введіть широту:").split('.')
-loc_2 = input("Введіть довготу:").split('.')
-loc_1 = int(loc_1[0]) + int(loc_1[1])/(10 ^ (len(loc_1[1])))
-loc_2 = int(loc_2[0]) + int(loc_2[1])/(10 ^ (len(loc_2[1])))
+    locations_path = input(
+        "Введіть шлях до файлу з фільмами(якщо це інший файл: він має мати таку ж конструкцію як і даний файл):")
+    world_path = input(
+        "Введіть шлях до файлу з населенням(якщо це інший файл: він має мати таку ж конструкцію як і даний файл):")
+    lst = input_from_file(locations_path, year)
+    map_1 = folium.Map(location=[loc_1, loc_2], zoom_start=1000)
 
-locations_path = input(
-    "Введіть шлях до файлу з фільмами(якщо це інший файл: він має мати таку ж конструкцію як і даний файл):")
-world_path = input(
-    "Введіть шлях до файлу з населенням(якщо це інший файл: він має мати таку ж конструкцію як і даний файл):")
-lst = input_from_file(locations_path, year)
-print(len(lst))
-map_1 = folium.Map(location=[loc_1, loc_2], zoom_start=50)
-lst = lst[:50]
+    lst = lst[:50]
 
-geolocator = Nominatim(user_agent="specify_your_app_name_here", timeout=3)
-result_lst = []
-for i in lst:
-    try:
-        result_lst.append([i[0], geolocator.geocode(i[2]).latitude, geolocator.geocode(
-            i[2]).longitude])
-    except:
-        pass
-result_lst.sort(key=lambda x: haversine.haversine(
-    (loc_1, loc_2), (x[1], x[2])))
-result_lst = result_lst[:10]
+    geolocator = Nominatim(user_agent="specify_your_app_name_here", timeout=3)
+    result_lst = []
+    for i in lst:
+        try:
+            result_lst.append([i[0], geolocator.geocode(i[2]).latitude, geolocator.geocode(
+                i[2]).longitude])
+        except:
+            pass
+    result_lst.sort(key=lambda x: haversine.haversine(
+        (loc_1, loc_2), (x[1], x[2])))
+    result_lst = result_lst[:10]
 
 
-fg = folium.FeatureGroup(name=year + "- films")
-for name, lt, ln in result_lst:
-    fg.add_child(folium.Marker(
-        location=[lt, ln], popup=name, icon=folium.Icon()))
-map_1.add_child(fg)
+    fg = folium.FeatureGroup(name=year + "- films")
+    for name, lt, ln in result_lst:
+        fg.add_child(folium.Marker(
+            location=[lt, ln], popup=name, icon=folium.Icon()))
+    map_1.add_child(fg)
 
-fg_pp = folium.FeatureGroup(name="Population")
-fg_pp.add_child(folium.GeoJson(data=open(world_path, 'r', encoding='utf-8-sig').read(),
-                               style_function=lambda x: {'fillColor': 'green'
-                                                         if x['properties']['POP2005'] < 10000000 else 'orange'
-                                                         if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
-map_1.add_child(fg_pp)
-map_1.add_child(folium.LayerControl())
+    fg_pp = folium.FeatureGroup(name="Population")
+    fg_pp.add_child(folium.GeoJson(data=open(world_path, 'r', encoding='utf-8-sig').read(),
+                                style_function=lambda x: {'fillColor': 'green'
+                                                            if x['properties']['POP2005'] < 10000000 else 'orange'
+                                                            if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+    map_1.add_child(fg_pp)
+    map_1.add_child(folium.LayerControl())
 
-map_1.save('Map_1.html')
+    map_1.save('Map_1.html')
+    print("Map is generating...")
+    print("Please wait...")
+    print("Finished. Please have look at the map Map_1.html")
+except:
+    print("Введіть правильні дані")
